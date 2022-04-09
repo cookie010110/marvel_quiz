48e444819c8b088ac551d3f682c582f8ad209c31
Alexis Cooke <alexiscooke265@gmail.com>
2022-04-09 13:31:22 -0400

    updating quiz.py

---
+++ quiz.py
@@ -0,0 +1,8 @@
+from colorama import Fore, Back, Style
+from emoji import emojize
+
+ = ["Gamora", "Groot", "Star-Lord", "Rocket Raccoon"]
+
+player_choice = input("Choose Gamora, Groot, Star-Lord, or Rocket Raccoon: ")
+
+print("user chose " + player_choice)
